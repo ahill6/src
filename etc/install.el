9e014a7ade2e8dd6b4106c24000645066a21b0d0
Tim Menzies <tim@menzies.us>
2016-08-08 01:06:03 -0400

    saving

--- etc/install.el
+++ etc/install.el
@@ -3,11 +3,13 @@
 (require 'package)
 
 ; find package information from following archives
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
 
-(package-initialize)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+
+        (package-initialize)
+
 
 (mapcar (lambda (package)
           ; install package if not already installed
@@ -16,10 +18,6 @@
 
         ; list of packages to be installed
         '(zenburn-theme
-          material-theme
-          monokai-theme
-          leuven-theme
-          spacemacs-theme
           neotree
           powerline
           markdown-mode
