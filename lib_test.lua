842056ce8aacf20135e9e772211c2853defec51e
Jason Graffius <jasongraffius@gmail.com>
2016-08-25 16:40:54 -0400

    Changed test function suffix

--- lib_test.lua
+++ lib_test.lua
@@ -6,17 +6,17 @@ require "lib"
 
 -- Test for max:
 
-function _max()
+function max_test()
   assert(max(1,2) == 2)
-  assert(max(6,3) == 3)
+  assert(max(6,3) == 6)
   assert(max(23, 24) == 24)
-  assert(max('a','b' == 'b'))
+  assert(max('a','b') == 'b')
   assert(max(42.4, 42.51) == 42.51)
 end
 
 -- Test for min:
 
-function _min()
+function min_test()
   assert(min(1,2) == 1)
   assert(min(6,3) == 3)
   assert(min(23, 24) == 23)
@@ -26,7 +26,7 @@ end
 
 -- Test for abs:
 
-function _abs()
+function abs_test()
   assert(abs(5) == 5)
   assert(abs(-6) == 6)
   assert(abs(0) == 0)
