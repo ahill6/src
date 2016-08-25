67b2bc4c7d177da07fac198775c9967e9de7743a
Jason Graffius <jasongraffius@gmail.com>
2016-08-25 16:43:52 -0400

    Added test case to min/max tests

--- lib_test.lua
+++ lib_test.lua
@@ -7,21 +7,23 @@ require "lib"
 -- Test for max:
 
 function max_test()
-  assert(max(1,2) == 2)
-  assert(max(6,3) == 6)
+  assert(max(1, 2) == 2)
+  assert(max(6, 3) == 6)
   assert(max(23, 24) == 24)
-  assert(max('a','b') == 'b')
+  assert(max('a', 'b') == 'b')
   assert(max(42.4, 42.51) == 42.51)
+  assert(max(9.9, 10) ~= 9.9)
 end
 
 -- Test for min:
 
 function min_test()
-  assert(min(1,2) == 1)
-  assert(min(6,3) == 3)
+  assert(min(1, 2) == 1)
+  assert(min(6, 3) == 3)
   assert(min(23, 24) == 23)
-  assert(min('a','b') == 'a')
+  assert(min('a', 'b') == 'a')
   assert(min(42.4, 42.51) == 42.4)
+  assert(min(9.9, 10) ~= 10)
 end
 
 -- Test for abs:
